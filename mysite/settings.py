6e9298991aa850de94b855681df107021b275aa0
Your Name <you@example.com>
2020-06-29 20:29:51 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,6 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'home/g17911hm/g17911hm.pythonanywhere.com/static'
+STATIC_ROOT = '/home/g17911hm/djangogirls/blog/static'
 #STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_DIR = os.path.join(BASE_DIR,'static')
